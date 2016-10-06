9f55e9246bff781ad9111aa520e53c3e4629698c
Quyet Luu <huuquyet151091@gmail.com>
2016-10-07 02:09:33 +0700

    Add Android app

---
+++ Bluetooth_Aduino_Control/app/src/main/java/com/example/quyetluu/bluetooth_aduino_control/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.quyetluu.bluetooth_aduino_control;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(700);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(MainActivity.this,DeviceList.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
